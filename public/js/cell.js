e766fb9b387fb9006b5cdfcea0e4ec9c90d7997b
Kateryna Bondareva <kbondareva@gmail.com>
2017-06-14 21:05:03 -0400

    Set up cell object

---
+++ public/js/cell.js
@@ -0,0 +1,33 @@
+// Constructor Function
+function Cell(width, height, color){
+  this.width = width || 100;
+  this.height = height || 100;
+  this.color = color || 'red';
+}
+// const Cell = {
+//   width: 100,
+//   height: 200,
+//   color: 'red'
+// };
+// Factory function
+// function createCell(width, height, color){
+//   const Cell = {
+//     width: width,
+//     height: height,
+//     color: color
+//   };
+//   return Cell;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+/////
